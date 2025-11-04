e1b84e7eb041a8d08e3607b27ca13a85e2c33f6d
Razzinn <ig.razvan12@gmail.com>
2025-11-04 16:28:48 +0100

    fix

--- Art.e_proj/backend/server.js
+++ Art.e_proj/backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const path = require('path');
 const fs = require('fs');
-require('dotenv').config();
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 const PORT = process.env.PORT || 3002;
