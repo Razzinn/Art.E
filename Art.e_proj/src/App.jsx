a9f68b4d333737bfcb48a1f7f20c31aeb635cdc1
Razzinn <ig.razvan12@gmail.com>
2025-11-12 14:29:29 +0100

    svuotamento main

--- Art.e_proj/src/App.jsx
+++ Art.e_proj/src/App.jsx
@@ -1,42 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { LanguageProvider } from './contexts/LanguageContext';
-import NavbarSectionR from './components/NavbarSectionR';
-import HeroSectionR from './components/HeroSectionR';
-import Offers from './components/Offers';
-import Footer from './components/footer';
-import ScrollToTop from './components/ScrollToTop';
-import ServiceRequestPage from './pages/ServiceRequestPageNew';
-import NotFoundPage from './pages/NotFoundPage';
-import Design3DPage from './componentisecondarisezioni/Design3DPage';
-import AbbigliamentoPage from './componentisecondarisezioni/AbbigliamentoPage';
-import WebAppDesignPage from './componentisecondarisezioni/WebAppDesignPage';
-import IdeeRegalo from './componentisecondarisezioni/IdeeRegaloPage';
-import './App.css';
-
 function App() {
   return (
-    <LanguageProvider>
-      <BrowserRouter>
-        <ScrollToTop />
-        <NavbarSectionR />
-        <Routes>
-          <Route path="/" element={
-            <>
-              <HeroSectionR />
-              <Offers />
-            </>
-          } />
-          <Route path="/servizi/:serviceSlug" element={<ServiceRequestPage />} />
-          <Route path="/stampa-3d" element={<Design3DPage />} />
-          <Route path="/abbigliamento" element={<AbbigliamentoPage />} />
-          <Route path="/webapp-design" element={<WebAppDesignPage />} />
-          <Route path="/idee-regalo" element={<IdeeRegalo />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </LanguageProvider>
-  );
+    <div>
+      <h1>React App</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
